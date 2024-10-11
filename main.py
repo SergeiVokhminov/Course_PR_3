c553b30a42f492b5c41da7254f3991eed3a88a03
sergei_vokhminov <rreaper@list.ru>
2024-10-11 22:44:32 +0300

    обновлены модули api_wor и main

--- main.py
+++ main.py
@@ -1,15 +1,162 @@
-import datetime
+from datetime import datetime, date
+
+from config import config
+from src.api_work import HeadHunterApi
+from src.db_class import DBManager
+from src.db_create import create_db
+from src.db_save_info import save_info_employer_database
+from src.employers_class import Employer
+from src.file_work import JobFile
+from src.utils import prints_a_greeting, get_employers_id
+from src.vacancies_class import Vacancy
 
 
 def main():
-    """Главная функция объединяет работу всех созданных функций."""
+    """
+    Функция соединяет работу всех реализованных функций.
+    :return: Результат работы реализованных функций.
+    """
 
-    data_now = datetime.date.today()
-    datetime_now = datetime.datetime.now()
-    time_now = datetime_now.time()
-    print(data_now)
-    print(time_now)
+    # приветствие с пользователем
+    prints_a_greeting()
+
+    # Создание экземпляра класса для работы с API
+    hh_api = HeadHunterApi()
+    # print(hh_api)
+
+    # получение информации о работодателях
+    employer = hh_api.get_info_employers()
+    # print(employer)
+    emp = hh_api.get_load_emp(employer)
+    # print(emp)
+
+    print(
+        f"Мне удалось найти информацию о более {len(employer)} работодателях.\n"
+        f"Из-за большого количества работодателей и вакансий,\n"
+        f"оставляю только 10 компаний у которых больше всего объявлений о вакансиях.\n"
+    )
+
+    # оставляем 10 работодателей по большему количеству открытых вакансий
+    top_employers = emp[:10]
+    # print(top_employers)
+    # print(len(top_employers))
+
+    # получаем список id работодателей
+    id_employers_list = get_employers_id(top_employers)
+    print(id_employers_list)
+    # print(type(id_employers_list))
+
+    # save_file = JobFile()
+    # save_file.save_vacancy_file(top_employers)
+
+    # read_file = save_file.read_file()
+    # print(read_file)
+
+    # получаем список вакансий по id работодателя
+    vacancies_employer = hh_api.get_vacancies_employer(id_employers_list)
+    print(len(vacancies_employer))
+
+    # получение сокращенной информации о вакансиях работодателей
+    user_vac_emp = hh_api.get_load_vac_emp(vacancies_employer)
+
+    save_emp_file = JobFile("employers.json")
+    save_vac_emp_file = JobFile("vacancies.json")
+    # save_file.save_vacancy_file(user_vac_emp)
+
+    # read_file = save_file.read_file()
+
+    print(len(user_vac_emp))
+
+    print("Созданию базу данных db_test")
+    params = config()
+    create_db("db_test", params)
+    #
+    print("База данных db_test создана.")
+    print("Сохраняем информацию о работодателях в базу данных.")
+    save_info_employer_database(
+        top_employers,
+        user_vac_emp,
+        "db_test",
+        params,
+    )
+
+    print(
+        """Что сделать с полученными данными?
+    1. Вывести название компании и количество вакансий.
+    2. Вывести информацию о вакансиях.
+    3. Вывести среднюю зарплату по вакансиям.
+    4. Вывести все вакансии у которых зарплата выше средней.
+    5. Вывести вакансии по ключевому слову.
+    6. Записать данные в файл.
+    0. Выйти и сохранить полученные данные в файл."""
+    )
+
+    while True:
+        user_answers = input("Введите цифру: \n")
+        if user_answers in ("1", "2", "3", "4", "5", "6", "0"):
+            break
+        else:
+            print("Введён некорректный ответ. Повторите ввод.")
+
+    menu = {
+        "1": "Вывести название компании и количество вакансий.",
+        "2": "Вывести информацию о вакансиях.",
+        "3": "Вывести среднюю зарплату по вакансиям.",
+        "4": "Вывести все вакансии у которых зарплата выше средней.",
+        "5": "Вывести вакансии по ключевому слову.",
+        "6": "Записать данные в файл.",
+        "0": "Выйти и удалить данные из файл.",
+    }
+
+    print(f"Для обработки выбрано: {menu.get(user_answers)}\n")
+
+    user_db = DBManager("db_test", params)
+
+    # выводим компании и количество вакансий
+    if user_answers == "1":
+        all_emp = user_db.get_companies_and_vacancies_count()
+        # print(all_emp)
+        for key, value in all_emp:
+            print(f'"Название компании": {key} | "Количество вакансий": {value} шт.')
+
+    elif user_answers == "2":
+        all_vac = user_db.get_all_vacancies()
+        for i in all_vac:
+            print(
+                f"Название компании - {i[0]}\nНазвание вакансии - {i[1]}\n"
+                f"Ссылка на вакансию - {i[2]}\nЗарплата - {i[3]}\n"
+            )
+
+    elif user_answers == "3":
+        salary_vac = user_db.get_avg_salary()
+        print(salary_vac)
+
+    elif user_answers == "4":
+        salary_vac = user_db.get_vacancies_with_higher_salary()
+        print(Vacancy.cast_to_object_list(salary_vac))
+
+    elif user_answers == "5":
+        user_input = input("Введите слово для поиска:\n")
+        salary_vac = user_db.get_vacancies_with_keyword(user_input)
+        print(salary_vac)
+        print(len(salary_vac))
+
+    elif user_answers == "6":
+        save_emp_file.save_vacancy_file(top_employers)
+        save_vac_emp_file.save_vacancy_file(user_vac_emp)
+        print("Данные записаны.\nРабота программы завершена.")
+
+    elif user_answers == "0":
+        save_emp_file.delete_file()
+        save_vac_emp_file.delete_file()
+        print("Работа программы завершена.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+# count_employers = 0
+#     for item in Employer.cast_to_object_list(top_employers):
+#         print(item)
+#         count_employers += 1
+#     print(count_employers)
--- src/api_work.py
+++ src/api_work.py
@@ -8,19 +8,18 @@ class HeadHunterApi(Parser):
 
     def __init__(self) -> None:
         """Инициализатор класса HeadHunterApi."""
-        self.__url_info_employers = "https://api.hh.ru/employers/"  # получения полной информации о работодателях
+        self.__url_info_employers = "https://api.hh.ru/employers"  # получения полной информации о работодателях
         self.__url_vacancies_employers = "https://api.hh.ru/vacancies"  # получение вакансий работодателей
         self.__headers = {"User-Agent": "HH-User-Agent"}
         self.__params_emp = {"page": 0, "per_page": 100, "sort_by": "by_vacancies_open"}
-        self.__params_vac_emp = {"page": 0, "per_page": 100, "area": 113, 'only_with_salary': True}
-        self.__employers: list[dict] = []
-        self.__vacancies_list: list[dict] = []
+        self.__params_vac_emp = {"page": 0, "per_page": 100, "area": 113, "only_with_salary": True}
+        self.__employers_list: list[dict] = []
+        self.__vacancies_employer_list: list[dict] = []
 
     def __api_connect(self) -> requests.Response:
         """Метод проверки подключения к API hh.ru"""
         response = requests.get(self.__url_info_employers, headers=self.__headers, params=self.__params_emp)
         if response.status_code == 200:
-            # print(response)
             return response
         else:
             print("Ошибка при получении данных", response.status_code)
@@ -31,63 +30,63 @@ class HeadHunterApi(Parser):
             response = self.__api_connect()
             if response:
                 employers = response.json()["items"]
-                self.__employers.extend(employers)
+                self.__employers_list.extend(employers)
                 self.__params_emp["page"] += 1
             else:
                 break
-        return self.__employers
+        return self.__employers_list
 
     def get_load_emp(self, __employers: list[dict]):
-        """Метод получения информации о работодателях по ключам."""
+        """Метод получения информации о работодателях по определенным ключам."""
         emp_list = []
 
-        if self.__employers:
-            # получение списка словарей с ключами id, name, url, vac_url, open_vacancies
-            for emp in self.__employers:
+        if self.__employers_list:
+            for emp in self.__employers_list:
                 emp_id = emp.get("id")
                 name = emp.get("name")
                 url = emp.get("alternate_url")
                 open_vacancies = emp.get("open_vacancies")
-
                 emp_list.append(
-                    {
-                        "id": emp_id,
-                        "name": name,
-                        "url": url,
-                        "open_vacancies": open_vacancies
-                    }
+                    {"employer_id": emp_id, "employer_name": name, "employer_url": url, "open_vacancies": open_vacancies}
                 )
-
-            return emp_list
+        return emp_list
 
     def get_vacancies_employer(self, employer_id) -> list[dict]:
         """Метод получения вакансий по id работодателя."""
-        self.__params_vac_emp['employer_id'] = employer_id
-        while self.__params_vac_emp.get("page") != 20:
-            response = requests.get(self.__url_vacancies_employers, headers=self.__headers, params=self.__params_vac_emp)
+        # self.__params_vac_emp["employer_id"] = employer_id
+        # while self.__params_vac_emp.get("page") != 1:
+        for item in employer_id:
+            url = f"{self.__url_vacancies_employers}?employer_id={item}"
+            # print(url)
+            response = requests.get(url, headers=self.__headers, params=self.__params_vac_emp)
             if response:
                 vacancies_employers = response.json()["items"]
-                self.__vacancies_list.extend(vacancies_employers)
-                self.__params_vac_emp["page"] += 1
+                self.__vacancies_employer_list.extend(vacancies_employers)
+                # self.__params_vac_emp["page"] += 1
             else:
                 break
-        return self.__vacancies_list
+        return self.__vacancies_employer_list
 
     def get_load_vac_emp(self, __vacancies_list):
-        """Метод получения списка словарей по ключам."""
+        """Метод получения списка вакансий работодателй по определенным ключам."""
         vac_emp_list = []
 
-        if self.__vacancies_list:
-            # получение списка словарей с ключами id, name, open_vacancies
-            for vacancy in self.__vacancies_list:
+        if self.__vacancies_employer_list:
+            for vacancy in self.__vacancies_employer_list:
                 id_vac = vacancy.get("id")
-                id_emp = vacancy.get('employer').get('id')
                 name_vac = vacancy.get("name")
+                employer_id = vacancy.get("employer").get("id")
                 name_emp = vacancy.get("employer").get("name")
                 url = vacancy.get("alternate_url")
-                requirement = vacancy.get("snippet").get("requirement")
+                if vacancy.get("snippet").get("requirement"):
+                    requirement = vacancy.get("snippet").get("requirement")
+                else:
+                    requirement = "Данные отсутствуют"
                 responsibility = vacancy.get("snippet").get("responsibility")
-                area = vacancy.get("area").get("name")
+                if vacancy.get("area").get("name"):
+                    area = vacancy.get("area").get("name")
+                else:
+                    area = "Данные отсутствуют"
                 if vacancy.get("salary"):
                     if vacancy["salary"]["to"]:
                         salary = vacancy["salary"]["to"]
@@ -98,15 +97,15 @@ class HeadHunterApi(Parser):
 
                 vac_emp_list.append(
                     {
-                        'vacancy_id': id_vac,
-                        'employer_id': id_emp,
-                        'name_vacancy': name_vac,
-                        'name_employer': name_emp,
-                        'url': url,
-                        'requirement': requirement,
-                        'responsibility': responsibility,
-                        'area': area,
-                        'salary': salary,
+                        "vacancy_id": id_vac,
+                        "employer_id": employer_id,
+                        "vacancy_name": name_vac,
+                        "employer_name": name_emp,
+                        "url": url,
+                        "requirement": requirement,
+                        "responsibility": responsibility,
+                        "area": area,
+                        "salary": salary,
                     }
                 )
 
@@ -114,28 +113,32 @@ class HeadHunterApi(Parser):
 
 
 if __name__ == "__main__":
-    user_list_company = [
-        {'id': '966757', 'name': 'Группа КОМФОРТ'},
-        {'id': '4344489', 'name': 'Точка Пересечения'}
-    ]
-
     hh_api = HeadHunterApi()
     #  получаем всю информацию о работодателях
     emp_info = hh_api.get_info_employers()
     # print(emp_info)
     # print(len(emp_info))
+    # print(type(emp_info))
+    # print()
 
     #  получаем сокращенную информацию о работодателях
-    list_emp = hh_api.get_load_emp(emp_info)[:10]
-    # print(list_emp)
-    # print(len(list_emp))
+    user_list_employers = hh_api.get_load_emp(emp_info)
+    # print(user_list_employers)
+    # print(type(user_list_employers))
+    # print(len(user_list_employers))
+    # print()
 
-    # получаем список вакансий работодателя
-    vacancies = hh_api.get_vacancies_employer(["3148"])
+    user_id_list = ("3148", "2060086", "4623486")
+
+    #  получаем вакансии работодателя по его id
+    vacancies = hh_api.get_vacancies_employer(user_id_list)  # "2060086", "4623486"
     # print(vacancies)
     # print(len(vacancies))
+    # print(type(vacancies))
+    # print()
 
     # получаем сокращенную информации о вакансиях работодателя
     list_vacancies = hh_api.get_load_vac_emp(vacancies)
     # print(list_vacancies)
     # print(len(list_vacancies))
+    # print(type(list_vacancies))
